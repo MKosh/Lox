8cca65af19feed5689e22e847d4d392ffb0652d4
Mark Mekosh <markmekosh@gmail.com>
2025-09-11 21:22:06 -0400

    Starting chapter 10

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -120,6 +121,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return (double)left * (double)right;
     }
     return null;
+  }
+
+  @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    LoxCallable function = (LoxCallable)callee;
+    return function.call(this, arguments);
   }
 
   private Object evaluate(Expr expr) {
