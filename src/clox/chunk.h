9c24433a81667897448383525c2b23148708f191
Mark Mekosh <markmekosh@gmail.com>
2025-09-17 12:16:22 -0400

    Finished chapter 14 section 4

--- src/clox/chunk.h
+++ src/clox/chunk.h
@@ -3,4 +3,18 @@
 
 #include "common.h"
 
+typedef enum {
+  OP_RETURN
+} OpCode;
+
+typedef struct {
+  i32 count;
+  i32 capacity;
+  u8* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, u8 byte);
+
 #endif
