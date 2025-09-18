fb2e90f9cbe9085b9f6bddd93f3cd6a85938b42c
Mark Mekosh <markmekosh@gmail.com>
2025-09-18 14:30:08 -0400

    Finished chapter 14

--- src/clox/chunk.c
+++ src/clox/chunk.c
@@ -7,20 +7,31 @@ void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  chunk->lines = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(u8, chunk->code, chunk->capacity);
+  FREE_ARRAY(i32, chunk->lines, chunk->capacity);
+  freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
 
-void writeChunk(Chunk *chunk, u8 byte) {
+void writeChunk(Chunk *chunk, u8 byte, i32 line) {
   if (chunk->capacity  < chunk->count + 1) {
     i32 oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(u8, chunk->code, oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(i32, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
+  chunk->lines[chunk->count] = line;
   chunk->count++;
+}
+
+i32 addConstant(Chunk* chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
 }
--- src/clox/chunk.h
+++ src/clox/chunk.h
@@ -2,19 +2,24 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
-  OP_RETURN
+  OP_CONSTANT,
+  OP_RETURN,
 } OpCode;
 
 typedef struct {
   i32 count;
   i32 capacity;
   u8* code;
+  i32* lines;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, u8 byte);
+void writeChunk(Chunk* chunk, u8 byte, i32 line);
+i32 addConstant(Chunk* chunk, Value value);
 
 #endif
